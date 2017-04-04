79cb3d0cb7773b0e17b694db776bd50023ac20a0
Jasmin Reckendorfer <jasmin.reckendorfer@gmail.com>
2017-04-04 14:57:16 +0200

    add test cases

--- bruch/Unit_Zusatz.py
+++ bruch/Unit_Zusatz.py
@@ -1,8 +1,3 @@
-"""
-Created on 27.12.2013
-
-@author: uhs374h
-"""
 import unittest
 
 from bruch.Bruch import *
@@ -14,27 +9,45 @@ class TestAllgemein(unittest.TestCase):
         self.b2 = Bruch(self.b)
         self.b3 = Bruch(4, 2)
         pass
-
+    
     def tearDown(self):
         del self.b, self.b2, self.b3
         pass
 
+    """
+    Einen tuple (gleiche) Brüche erstellen und vergleichen
+    """
     def testTuple(self):
         z, n = Bruch(3, 4)
         assert (z == 3 and n == 4)
 
+    """
+    Einen tuple (gleiche) Brüche erstellen, daraus einen Bruch erstellen und dann vergleichen
+    """
     def testTuple2(self):
         z, n = self.b
         self.assertEqual(Bruch(z, n), self.b)
 
-    def testTuple3_Error(self):
-        b3 = list(self.b2)
-        self.assertRaises(IndexError, self.tryIndex, b3, 3)
-
-    @staticmethod
-    def tryIndex(obj, index):
-        return obj[index]
-
+    """
+    Brüche mit dem *= operator multiplizieren
+    """
+    def testMulti2(self):
+        self.b *= Bruch(2)
+        assert (self.b == 3)
+        
+    """
+    Brüche mit dem *= operator multiplizieren
+    """
+    def testMulti2(self):
+        self.b *= Bruch(2)
+        assert (self.b == 3)
+        
+    """
+    Brüche mit dem += operator addieren
+    """
+    def testiAdd2(self):
+        self.b += Bruch(1)
+        assert (self.b == Bruch(5, 2))
 
 if __name__ == "__main__":
     unittest.main()
